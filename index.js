e82bae87debe226e24b136cfb70b9e53f9951f70
s-prianto <prianto2796@gmail.com>
2020-02-15 22:27:39 +0700

    Tugas 36 JS

---
+++ index.js
@@ -0,0 +1,6 @@
+function jalan(){
+	console.log(document.getElementById('email'));
+	console.log(document.getElementsByTagName('h2'));
+	console.log(document.getElementsByClassName('password'));
+	console.log(document.querySelector('.department'));
+}
